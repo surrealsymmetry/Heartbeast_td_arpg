c8d0e7493383376843710748a090b37773011232
sev <severedspaces@gmail.com>
2023-03-20 21:44:30 -0230

    refactor meter to node

--- UI/meter.gd
+++ UI/meter.gd
@@ -1,51 +1,67 @@
 extends Node2D
 
+var parent :Node 
+var test_value :float = Util.random_float_normal()
+
+var pos :Vector2
+var size :Vector2
+
+var pad :Vector2 = Vector2(1, 1)
+
+var bg_color :Color = Color(0, 0, 0, 0.65)
+#var fg_color_low = Color.from_hsv(0.0, 1.0, 1.0, 1.0)
+#var fg_color_high = Color.from_hsv(100.0, 1.0, 0.8, 1.0)
+var hue :float
+var gradient :Gradient
+
+var font :Font = load("res://Fonts/XanhMono-Italic.otf")
+var font_size :int = 4
+
+var _decreasing :bool = false
+var _cycles_per_second = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	gradient = Gradient.new()
+	
+	gradient.set_color(0, Color.RED)
+	gradient.set_color(1, Color.FOREST_GREEN)
+	
+	gradient.add_point(0.15, Color.RED)
+	gradient.add_point(0.35, Color.ORANGE_RED)
+	gradient.add_point(0.45, Color.DARK_ORANGE)
+	gradient.add_point(0.75, Color.FOREST_GREEN)
+	
+	for i in range(0, gradient.get_point_count() - 1):
+		print("%s: %s" % [i, gradient.get_color(i)])
+	
+	
+	parent = get_parent()
+	size = get_size()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:	
+	pos = parent.get_placement( )
+	_test_range( delta )
+	queue_redraw()
 
-func draw_meter(text, f):
-	pass
-#	var bg_color :Color = Color(0, 0, 0, 0.5)
-#	var fg_color :Color = Color.RED.lerp(Color.GREEN, f)
-#	var pad = 1
-#	var w = 75
-#
-#	var line_count = label.get_line_count()
-#	var line_height = label.get_line_height()
-#	data.merge({"LINES: ": line_count}, true)
-#
-#	var x = label.position.x
-#	var y = line_height * line_count
-#
-#	var meter_text_pos = Vector2(x, y)
-#
-#	var range :Vector2 = Vector2(0, 1)
-#
-#	draw_rect(
-#		Rect2(
-#			x, 
-#			y, 
-#			w, 
-#			line_height
-#		), 
-#		bg_color 
-#	)
-#	draw_rect( 
-#		Rect2(
-#			x + pad, 
-#			y + pad, 
-#			w * f - pad * 2, 
-#			line_height - pad * 2 
-#		), 
-#		fg_color
-#	)
-#
-#	draw_string_outline(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4, 2, Color.BLACK)
-#	draw_string(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4 )
+func _draw():
+	draw_rect( Rect2(pos,size + pad), bg_color )
+	draw_rect( Rect2(pos + pad, Vector2(size.x * test_value,  size.y) - pad),  gradient.sample(test_value))
+	
+	draw_string_outline( font, pos, "example", HORIZONTAL_ALIGNMENT_LEFT, -1, font_size, 2, Color.BLACK )	
+	draw_string( font, pos, "example", HORIZONTAL_ALIGNMENT_LEFT, -1, font_size )
+
+func get_size() -> Vector2:
+	var h = font.get_height(font_size)
+	return Vector2(30, h - pad.y)
+
+func _test_range( delta ) -> void:
+	if not _decreasing:
+		test_value += _cycles_per_second * delta
+	else:
+		test_value -= _cycles_per_second * delta
+	if test_value <= 0.0:
+		_decreasing = false
+	if test_value >= 1.0:
+		_decreasing = true
--- _DebugHUD.gd
+++ _DebugHUD.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
 var data = {}
+var speed_meter = Node
 
-
-@onready var label : Node = $"Control/Label 1"
+@onready var label 	: Node = $"Control/Label 1"
 @onready var player : CharacterBody2D = get_tree().get_root().get_node("World/Player")
+@onready var meter_scene :PackedScene = load("res://UI/meter.tscn")
 
-@export var font : Font
 
 # VIRTUAL FUNCTIONS#
 ####################
+func _ready() -> void:
+	speed_meter = meter_scene.instantiate()
+	add_child(speed_meter)
+	
+#	var line_count = label.get_line_count()
+#	var line_height = label.get_line_height()
+#	data.merge({"LINES: ": line_count}, true)
+#
+#	var x = label.position.x
+#	var y = line_height * line_count
+
 func _process(delta: float) -> void:
 	data.merge( {
 			"FPS: ": Engine.get_frames_per_second(),
@@ -17,12 +28,12 @@ func _process(delta: float) -> void:
 		}, true
 	)
 	
-	test_label_adding( delta )
+#	test_label_adding( delta )
 	queue_redraw()
 	
-func _draw() -> void:
-	draw_meter("test meter", Util.random_float_normal())
-	
+#func _draw() -> void:
+#	draw_meter("test meter", Util.random_float_normal())
+
 func _on_player_debug( dict: Dictionary ) -> void:
 	data.merge( dict, true )
 	update_label()
@@ -36,45 +47,48 @@ func update_label():
 	for key in sorted_keys:
 		label.text += "\n%s:\t%s" % [key, data[key]]
 
-
-func draw_meter(text, f):
-	var bg_color :Color = Color(0, 0, 0, 0.5)
-	var fg_color :Color = Color.RED.lerp(Color.GREEN, f)
-	var pad = 1
-	var w = 75
-	
-	var line_count = label.get_line_count()
-	var line_height = label.get_line_height()
-	data.merge({"LINES: ": line_count}, true)
-	
-	var x = label.position.x
-	var y = line_height * line_count
-	
-	var meter_text_pos = Vector2(x, y)
-	
-	var range :Vector2 = Vector2(0, 1)
-
-	draw_rect(
-		Rect2(
-			x, 
-			y, 
-			w, 
-			line_height
-		), 
-		bg_color 
-	)
-	draw_rect( 
-		Rect2(
-			x + pad, 
-			y + pad, 
-			w * f - pad * 2, 
-			line_height - pad * 2 
-		), 
-		fg_color
-	)
-	
-	draw_string_outline(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4, 2, Color.BLACK)
-	draw_string(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4 )
+func get_placement():
+	var y = label.get_line_count() * label.get_line_height()	
+	var x = 2
+	return Vector2(x, y)
+#func draw_meter(text, f):
+#	var bg_color :Color = Color(0, 0, 0, 0.5)
+#	var fg_color :Color = Color.RED.lerp(Color.GREEN, f)
+#	var pad = 1
+#	var w = 75
+#
+#	var line_count = label.get_line_count()
+#	var line_height = label.get_line_height()
+#	data.merge({"LINES: ": line_count}, true)
+#
+#	var x = label.position.x
+#	var y = line_height * line_count
+#
+#	var meter_text_pos = Vector2(x, y)
+#
+#	var range :Vector2 = Vector2(0, 1)
+#
+#	draw_rect(
+#		Rect2(
+#			x, 
+#			y, 
+#			w, 
+#			line_height
+#		), 
+#		bg_color 
+#	)
+#	draw_rect( 
+#		Rect2(
+#			x + pad, 
+#			y + pad, 
+#			w * f - pad * 2, 
+#			line_height - pad * 2 
+#		), 
+#		fg_color
+#	)
+#
+#	draw_string_outline(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4, 2, Color.BLACK)
+#	draw_string(font, meter_text_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, 4 )
 	
 var _time_since_new_label :float = 0
 func test_label_adding(delta):
